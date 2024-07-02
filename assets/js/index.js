2a8ad674e8cd1a118a1b31c149a5dda8d772b28c
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-07-02 14:43:45 +0000

    Populate answers with custom answers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,9 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
-const shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -8,12 +11,17 @@ function startGame() {
     console.log('started')
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => Math.random() - .5)
-    questionContainerElement.remove('hide')
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {

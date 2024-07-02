e1c88bb004c6ea48e2b0acc8ee90ccb8f6e41c59
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-07-02 13:31:24 +0000

    Remove the hide class so the quiz questions are revealed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.remove('hide')
 }
 
 function setNextQuestion() {
